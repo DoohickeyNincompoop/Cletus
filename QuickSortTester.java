e68f242c3ee657a1afeff82b1986a9991e1923c5
Kerwin Chen <kchen37@stuy.edu>
2018-03-14 01:15:16 -0400

    data

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,13 +1,15 @@
 public class QuickSortTester
 {
     public static void main(String[] args){
-	for(int i = 1; i < 2000; i += 100){
+	for(int i = 1; i < 200000; i += 100){
 	    long past,current;
 	    int[] x = QuickSort.buildArray(i,i);
 	    QuickSort.shuffle(x);
 	    past = System.nanoTime();
 	    QuickSort.qsort(x);
 	    current = System.nanoTime();
+	    System.out.print(i);
+	    System.out.print(" , ");
 	    System.out.println(current - past);
 	}
     }
